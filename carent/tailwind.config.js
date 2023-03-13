b1eb6ad568d6910797e9fef0402d0d149163c641
mahdi bahrami <mahdibahrami40@gmail.com>
2023-03-13 23:27:08 +0330

    add file responsive web app

--- carent/tailwind.config.js
+++ carent/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "custom": '0 50px 25px -24px rgb(0 0 0 / 0.3)'
+      }
+    },
   },
   plugins: [],
 }
