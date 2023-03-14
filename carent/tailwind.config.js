d3b9712ca59162629d7eb7049e1064bbcaffdc29
mahdi bahrami <mahdibahrami40@gmail.com>
2023-03-14 12:03:13 +0330

    finish html css file projects

--- carent/tailwind.config.js
+++ carent/tailwind.config.js
@@ -3,9 +3,7 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      boxShadow: {
-        "custom": '0 50px 25px -24px rgb(0 0 0 / 0.3)'
-      }
+     
     },
   },
   plugins: [],
